3a2ee854ada0ea75d2ce630b8e1ed73a18ca7b33
Portella13 <51299871+Portella13@users.noreply.github.com>
2022-08-16 20:38:11 -0300

    Create enemy.h

---
+++ enemy.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <allegro5/allegro.h>
+
+class Enemy {
+public:
+    Enemy(int star_);
+    void init();
+    void update();
+    void render();
+    void dispose();
+
+    float toRadians(float angle){
+        return angle * 3.1415 / 180
+    }
+
+private:
+    int star;
+    float x;
+    float y;
+    float radius;
+
+    float speed;
+    float dx;
+    float dy;
+    float angle;
+
+    int lives;
+
+    ALLEGRO_COLOR color;
+    bool isOnScreen;
+};
