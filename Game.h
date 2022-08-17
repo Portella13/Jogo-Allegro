38fb1893901bcf93e9ca8b547eea02db53b5eaa7
Portella13 <51299871+Portella13@users.noreply.github.com>
2022-08-16 21:15:49 -0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,3 +1,23 @@
 #include "Constants.h"
 //#include "Background.h"
+#include "Player.h"
+#include "Enemy.h"
 
+#include <vector>
+
+class Game{
+  publick:
+    game();
+    void init();
+    void update();
+    void render();
+    void handleInput();
+    void dispose();
+  
+  private:
+    Background* background;
+  Player player;
+  
+  std::vector<Enemy*> enemies;
+};
+    
